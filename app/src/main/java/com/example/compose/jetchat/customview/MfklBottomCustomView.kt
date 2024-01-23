540b61af918001eb5767164c68660a9c1f140d8d
mfkldev <marcio.souza@zup.com.br>
2024-01-22 21:16:23 -0300

    create mfkl bottom custom view

---
+++ app/src/main/java/com/example/compose/jetchat/customview/MfklBottomCustomView.kt
@@ -0,0 +1,38 @@
+package com.example.compose.jetchat.customview
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.compose.jetchat.R
+
+class MfklBottomCustomView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+
+    private val leftButton: Button
+    private val rightButton: Button
+
+    init {
+        LayoutInflater.from(context).inflate(R.layout.mfkl_bottom_custom_view, this, true)
+        leftButton = findViewById(R.id.mfkl_bottom_cv_leftButton)
+        rightButton = findViewById(R.id.mfkl_bottom_cv_rightButton)
+    }
+
+    fun setLeftButtonListener(listener: OnClickListener) {
+        leftButton.setOnClickListener(listener)
+    }
+
+    fun setRightButtonListener(listener: OnClickListener) {
+        rightButton.setOnClickListener(listener)
+    }
+
+    fun setLeftButtonText(text: String) {
+        leftButton.text = text
+    }
+
+    fun setRightButtonText(text: String) {
+        rightButton.text = text
+    }
+}
